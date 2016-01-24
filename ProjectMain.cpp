437339300ebd7ec94a59b0bd0f3e5c4c6ee96e09
owl <believeinjiang@gmail.com>
2016-01-25 03:15:17 +0800

    test

---
+++ ProjectMain.cpp
@@ -0,0 +1,49 @@
+﻿
+#include "ProjectMain.h"
+
+String inputString = "";         // a string to hold incoming data
+boolean stringComplete = false;  // whether the string is complete
+int BT_x;
+void setup() {
+	Serial.begin(57600);
+}
+
+void loop() {
+	delay(10);
+	if (stringComplete) {
+		Serial.println("BT_x");
+		Serial.println(BT_x);
+
+		stringComplete = false;
+	}
+}
+
+void serialEvent() {
+	while (Serial.available()) {
+		char h=Serial.read();
+		if(h=='x')
+		{
+			char str[]="";
+			char b=0;
+			int i=0;
+
+			while(b!=';')
+			{
+				Serial.print("b");
+				Serial.println(b);
+				
+
+				b=Serial.read();
+				if(b!=-1)
+				{
+					str[i]=b;
+					i++;
+				}
+			}
+			BT_x=atoi(str)  ;
+		}
+		
+		stringComplete=true;
+	}
+}
+
---
+++ ProjectMain.h
@@ -0,0 +1,16 @@
+﻿/*
+ * ProjectMain.h
+ *
+ * Created: 2015/11/8 下午 07:32:53
+ *  Author: kuantiag
+ */ 
+
+
+#ifndef PROJECTMAIN_H_
+#define PROJECTMAIN_H_
+
+#include <Arduino.h>
+
+
+
+#endif /* PROJECTMAIN_H_ */
